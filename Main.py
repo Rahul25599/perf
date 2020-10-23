709b34273f021b5a9f10548fb3fc736b7991dd2c
Rahul25599 <73216359+Rahul25599@users.noreply.github.com>
2020-10-23 08:22:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+'''
+Created on 21-Oct-2020
+
+@author: lsail
+'''
+from com.shaft.PageInsights import PageInsights
+
+def main():
+    page =  PageInsights("https://www.amazon.com/","AIzaSyBbFCnfGdhN6rfxwdhxDZYWYj0JKR2dTIc")
+    page.getPageInsights()
+    
+if __name__ == "__main__":
+    main()
