987f5e10e35cfe788c47824055fcd3b1ec6489f8
Rahul25599 <73216359+Rahul25599@users.noreply.github.com>
2020-10-23 08:57:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,5 +9,7 @@ def main():
     page =  PageInsights("https://www.amazon.com/","AIzaSyBbFCnfGdhN6rfxwdhxDZYWYj0JKR2dTIc")
     page.getPageInsights()
     
+    ' Test
+    
 if __name__ == "__main__":
     main()
